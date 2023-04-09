cff477e34115498b5a3f9bea7cba402903233d3b
eturov <eugeneturov977@gmail.com>
2023-04-09 18:15:17 +0300

    modify test_items.py

--- test_items.py
+++ test_items.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+# import time
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
@@ -16,6 +17,7 @@ class TestLocalization:
 
     def test_availability_cart_button(self, browser):
         browser.get(link)
+        # time.sleep(30)
         actual_cart_button = check_exists_by_css(browser, '.btn-add-to-basket')
         expected_cart_button = True
         assert actual_cart_button == expected_cart_button, \
